b49158ce9774394e03b41646a4db086d808e3711
Lukizudo <104065917+Lukizudo@users.noreply.github.com>
2022-06-19 20:20:52 +0200

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,22 +1,25 @@
 var submitForm = document.getElementById("submit");
 var email = document.getElementById("email");
 var vorname = document.getElementById("vorname");
-/*submitForm.disabled = true;
+
+submit.disabled = true
+
 const validate = () => {
     if (email.value == "") {
-        submitForm.disabled = true
+        submit.disabled = true
     } else {
-        submitForm.disabled = false
+        submit.disabled = false
     }
 }
+
 email.addEventListener("keyup", (event) => {
     validate()
-})*/
+})
 
 submitForm.addEventListener("click", async (event) => {
     console.log('1')
     event.preventDefault()
-  const result = await databaseClient.insertInto(["email", "vorname"], [email.value, vorname.value])
+  const result = await databaseClient.insertInto("users", ["email", "vorname"], [email.value, vorname.value])
   if (result.error) {
     alert("Datenbank Fehler: " + JSON.stringify(result.error, null, 2))
   }
