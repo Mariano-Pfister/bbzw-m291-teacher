d28a5156ea12ec740b9ea7bbd8b1dea075c15878
Mariano-Pfister <105049799+Mariano-Pfister@users.noreply.github.com>
2022-06-19 19:52:14 +0200

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,17 +1,27 @@
 var submitForm = document.getElementById("submit");
 var email = document.getElementById("email");
-
-
+/*submitForm.disabled = true;
+const validate = () => {
+    if (email.value == "") {
+        submitForm.disabled = true
+    } else {
+        submitForm.disabled = false
+    }
+}
+email.addEventListener("keyup", (event) => {
+    validate()
+})*/
 
 submitForm.addEventListener("click", async (event) => {
     console.log('1')
     event.preventDefault()
-    const result = await databaseClient.insertInto("users", ["email"], [email.value])
-    if (result.error) {
-        alert("Datenbank Fehler: " + JSON.stringify(result.error, null, 2))
-    }
-    else {
-        // Weiterleitung auf die Game Page
-        location.href = "./game.html"
-    }
+  const result = await databaseClient.insertInto("users", ["email"], [email.value])
+  if (result.error) {
+    alert("Datenbank Fehler: " + JSON.stringify(result.error, null, 2))
+  }
+  else {
+    // Weiterleitung auf die Game Page
+    location.href = "./game.html"
+  }
+
 })
