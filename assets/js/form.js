d807f8c3113d4bd17475b870cda5f512d71bee78
Mariano-Pfister <105049799+Mariano-Pfister@users.noreply.github.com>
2022-06-19 17:36:23 +0200

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -13,6 +13,77 @@ const validate = () => {
     } else {
         submitForm.disabled = false
     }
+    // Validiere Vorname
+    if (vorname == "") {
+        submitForm.disabled = true
+    } else {
+        var regex = /^[a-zA-Z äÄöÖüÜ]{1,}$/;
+        if (regex.test(vorname) === false) {
+            printError("vornameErr", "Bitte keine Zahlen und Sonderzeichen");
+        } else {
+            printError("vornameErr", "");
+            vornameErr = false;
+        }
+    }
+    // Validiere Nachname
+    if (nachname == "") {
+        submitForm.disabled = true
+    } else {
+        var regex = /^[a-zA-Z äÄöÖüÜ]{1,}$/;
+        if (regex.test(nachname) === false) {
+            submitForm.disabled = true
+        } else {
+            submitForm.disabled = false
+        }
+    }
+
+    // Validiere Adresse
+    if (ort == "") {
+        submitForm.disabled = true
+    } else {
+        var regex = /^[a-zA-Z äÄöÖüÜ 0-9 ]{2,}$/;
+        if (ort.length > 50) {
+            submitForm.disabled = true
+        } else {
+            submitForm.disabled = false
+        }
+    }
+
+    // Validiere PLZ
+    if (plz == "") {
+        submitForm.disabled = true
+    } else {
+        var regex = /^[0-9]{4,}$/;
+        if (plz.length !== 4) {
+            submitForm.disabled = true
+        } else {
+            submitForm.disabled = false
+        }
+    }
+
+    // Validiere email Adresse
+    if (email == "") {
+        submitForm.disabled = true
+    } else {
+        var regex = /^[0-9._%+-]+@[a-z0-9.-äÄöÖüÜ]+\.[a-z]{2,4}$/;
+        if (email.length > 50) {
+            submitForm.disabled = true
+        } else {
+            submitForm.disabled = false
+        }
+    }
+
+    // Validiere mobile Nummer
+    if (tel == "") {
+        submitForm.disabled = true
+    } else {
+        var regex = /^[0-9 +]{5,}$/;
+        if (tel.length > 15) {
+            submitForm.disabled = true
+        } else {
+            submitForm.disabled = false
+        }
+    }
 }
 email.addEventListener("keyup", (event) => {
     validate()
