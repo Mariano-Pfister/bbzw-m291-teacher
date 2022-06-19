68d1c29fff63cc3dbe3b8753affcbc2401c56f21
Mariano-Pfister <105049799+Mariano-Pfister@users.noreply.github.com>
2022-06-19 15:17:52 +0200

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,4 +1,35 @@
+
 const submit = document.getElementById("submit")
+const email = document.getElementById("email")
+submit.disabled = true
+
+const validate = () => {
+    if (email.value == "") {
+        submit.disabled = true
+    } else {
+        submit.disabled = false
+    }
+}
+
+email.addEventListener("keyup", (event) => {
+    validate()
+})
+
+
+submit.addEventListener("click", async (event) => {
+    event.preventDefault()
+    const result = await databaseClient.insertInto("users", ["email"], [email.value])
+    if (result.error) {
+        alert("Datenbank Fehler: " + JSON.stringify(result.error, null, 2))
+    }
+    else {
+        // Weiterleitung auf die Game Page  
+        location.href = "./game.html"
+    }
+
+})
+
+/*const submit = document.getElementById("submit")
 const email = document.getElementById("email")
 submit.disabled = true
 
@@ -50,7 +81,7 @@ function printError(elemId, hintMsg) {
     var vornameErr = nachnameErr = ortErr = plzErr = emailErr = telErr = agbErr = true;
   
     // Validiere Vorname
-      /*
+      
     if (vorname == "") {
       printError("vornameErr", "Bitte gib deinen Vornamen ein");
     } else {
